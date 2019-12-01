2f22e061b6c145502a2fafda4ff96b8b8e6348e4
Angelo-Valrentin <valrentin.angelo@gmail.com>
2019-12-01 11:14:23 +0900

    Implement all funtions in OfficialScorer
    Implemented functions to set SBO for pitching results.
    
    * bool calculatePitchingResult(PitchingResult pitchingResult);
    Calculate strike, ball, hits, out through pithing result
    
    * bool calculateStrikeOccurs();
    Calculate sbo count when strike occurs
    
    * bool calculateBallOccurs();
    Calculate sbo count when ball occurs
    
    * bool calculateHitOccurs();
    Calculate hits count when hits occurs
    
    * bool calculateOutOccurs();
    Calculate sbo count when out occurs

--- officialscorer.cpp
+++ officialscorer.cpp
@@ -16,38 +16,78 @@ OfficialScorer::~OfficialScorer()
 
 bool OfficialScorer::calculatePitchingResult(PitchingResult pitchingResult)
 {
-    //Todo: Implement logic to manipulate scoreboard based on pitching result.
+    bool bEndAtTheBat = false;
 
     if ( PitchingResult::STRIKE == pitchingResult ) {
-
+        bEndAtTheBat = calculateStrikeOccurs();
     }
     else if ( PitchingResult::BALL == pitchingResult ) {
-
+        bEndAtTheBat = calculateBallOccurs();
     }
     else if ( PitchingResult::HITS == pitchingResult ) {
-
+        bEndAtTheBat = calculateHitOccurs();
     }
     else if ( PitchingResult::OUT == pitchingResult ) {
-
+        bEndAtTheBat = calculateOutOccurs();
     }
+
+    return bEndAtTheBat;
 }
 
 bool OfficialScorer::calculateStrikeOccurs()
 {
-    //Todo: Implement logic for calculating sbo when the strike occurs.
+    bool bEndAtTheBat = false;
+    unsigned short nStrikeCount = m_pScoreBoard->getStrikeCount();
+    m_pScoreBoard->setStrikeCount(++nStrikeCount);
+
+    if ( static_cast<unsigned short>(SBMaximumCount::THREE_STRIKE) == nStrikeCount ) {
+        m_pScoreBoard->setStrikeCount(0);
+        m_pScoreBoard->setBallCount(0);
+
+        unsigned short nOutCount = m_pScoreBoard->getOutCount();
+        m_pScoreBoard->setOutCount(++nOutCount);
+
+        bEndAtTheBat = true;
+    }
+
+    return bEndAtTheBat;
 }
 
 bool OfficialScorer::calculateBallOccurs()
 {
-    //Todo: Implement logic for calculating sbo when the ball occurs.
+    bool bEndAtTheBat = false;
+    unsigned short nBallCount = m_pScoreBoard->getBallCount();
+    m_pScoreBoard->setBallCount(++nBallCount);
+
+    if ( static_cast<unsigned short>(SBMaximumCount::FOUR_BALL) == nBallCount ) {
+        m_pScoreBoard->setStrikeCount(0);
+        m_pScoreBoard->setBallCount(0);
+
+        unsigned short nHitsCount = m_pScoreBoard->getHitsCount();
+        m_pScoreBoard->setHitsCount(++nHitsCount);
+
+        bEndAtTheBat = true;
+    }
+
+    return bEndAtTheBat;
 }
 
 bool OfficialScorer::calculateHitOccurs()
 {
-    //Todo: Implement logic for increasement hits count when the hits occurs.
+    bool bEndAtTheBat = true;
+
+    unsigned short nHitsCount = m_pScoreBoard->getHitsCount();
+    m_pScoreBoard->setHitsCount(++nHitsCount);
+
+    return bEndAtTheBat;
 }
 
 bool OfficialScorer::calculateOutOccurs()
 {
-    //Todo: Implement logic for calculating sbo when the out occurs.
+    bool bEndAtTheBat = true;
+
+    unsigned short nOutCount = m_pScoreBoard->getOutCount();
+    m_pScoreBoard->setOutCount(++nOutCount);
+
+    return bEndAtTheBat;
 }
